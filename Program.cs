1c72304f9abb04d815a299cd81088e2cb5f5611c
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-10-05 21:02:34 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,20 +1,79 @@
-﻿using InputInterceptorNS;
+﻿using System.Text;
+using System.Timers;
+using InputInterceptorNS;
+using Spectre.Console;
 
 namespace KeyboardIntercept {
     internal class Program {
-        static void Main(string[] args) {
+        static class MainStatus {
+            public static bool active = false;
+            public static string textToWrite = "";
+        }
+        [STAThread]
+        public static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
             async void StartListening() {
-                Console.WriteLine("start");
-                await Task.Delay(3000);
-                Console.WriteLine("stop");
+                //Console.WriteLine("yes");
             }
 
             if (InitializeDriver()) {
+                // create hooks
                 KeyboardHook keyboardHook = new KeyboardHook(KeyboardCallback);
-                Console.WriteLine("Hooks enabled. Press any key to release.");
+                //Console.WriteLine("Hooks enabled. Press any key to release.");
+
                 char Key = '-';
+
+
+                // create live updating console text
+                Console.Clear();
+                AnsiConsole.Live(new Markup("Loading..."))
+                .StartAsync(async ctx => {
+                    while (true) {
+                        const int MAX_SHOWN_TEXT_TO_WRITE_LEN = 50;
+                        string textToWrite = MainStatus.textToWrite;
+                        if (textToWrite.Length > MAX_SHOWN_TEXT_TO_WRITE_LEN) {
+                            textToWrite = textToWrite.Substring(0, MAX_SHOWN_TEXT_TO_WRITE_LEN - 3) + "...";
+                        }
+                        var panel = new Spectre.Console.Panel(new Rows(
+                            new Markup($"[gold3]Intercept writing:[/] [{(MainStatus.active ? "green" : "red")}]{(MainStatus.active ? "active" : "inactive")}[/]"),
+                            new Markup($"[gold3]Text to write:[/] [darkorange]{textToWrite.EscapeMarkup()}[/]")
+                        ));
+                        panel.Header = new PanelHeader("[blue]Status[/]");
+                        panel.Border = BoxBorder.Rounded;
+                        panel.BorderStyle = new Style(Spectre.Console.Color.DarkCyan);
+                        //panel.Expand();
+                        panel.Width = 70;
+                        ctx.UpdateTarget(
+                            new Rows(
+                                panel,
+                                new Markup(" "),
+                                new Markup("[gold3]Press [magenta3]PageUp[/] to toggle [lightskyblue1]Intercept writing[/].[/]"),
+                                new Markup(" "),
+                                new Markup("[gold3]Press [magenta3]X[/] or [magenta3]Q[/] to [lightskyblue1]Stop[/] the program.[/]")
+                            )
+                        );
+                        ctx.Refresh();
+                        await Task.Delay(10);
+                    }
+                });
+
+                // Create a timer to get clipboard
+                System.Timers.Timer myTimer = new System.Timers.Timer();
+                myTimer.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs e) => {
+                    if (MainStatus.active) {
+                        return;
+                    }
+
+                    string clipboardText = GetClipboardData();
+                    MainStatus.textToWrite = clipboardText;
+                });
+                myTimer.Interval = 50;
+                myTimer.Enabled = true;
+
+
+
                 while (!(Char.ToLower(Key) == 'q' || Char.ToLower(Key) == 'x')) {
-                    Key = Console.ReadKey().KeyChar;
+                    Key = Console.ReadKey(true).KeyChar;
                 }
                 keyboardHook.Dispose();
             } else {
@@ -37,16 +96,19 @@ namespace KeyboardIntercept {
                     keyStroke.Code = KeyCode.LeftShift;
                     // Check if the key is released
                     if (keyStroke.State == KeyState.Up) {
-                        StartListening();
+                        MainStatus.active = !MainStatus.active;
+                        if (MainStatus.active) {
+                            StartListening();
+                        }
                     }
                 }
             }
 
             Boolean InitializeDriver() {
                 if (InputInterceptor.CheckDriverInstalled()) {
-                    Console.WriteLine("Input interceptor seems to be installed.");
+                    //Console.WriteLine("Input interceptor seems to be installed.");
                     if (InputInterceptor.Initialize()) {
-                        Console.WriteLine("Input interceptor successfully initialized.");
+                        //Console.WriteLine("Input interceptor successfully initialized.");
                         return true;
                     }
                 }
@@ -64,8 +126,32 @@ namespace KeyboardIntercept {
                         Console.WriteLine("Something... gone... wrong... :(");
                     }
                 } else {
-                    Console.WriteLine(
-                      "Restart program with administrator rights so it will be installed.");
+                    Console.WriteLine("Restart program with administrator rights so it will be installed.");
+                }
+            }
+
+            string GetClipboardData() {
+                try {
+                    string clipboardData = null;
+                    Exception threadEx = null;
+                    Thread staThread = new Thread(
+                            delegate () {
+                                try {
+                                    if (Clipboard.ContainsText(TextDataFormat.Text)) {
+                                        clipboardData = Clipboard.GetText(TextDataFormat.Text);
+                                    } else {
+                                        clipboardData = "";
+                                    }
+                                } catch (Exception ex) {
+                                    threadEx = ex;
+                                }
+                            });
+                    staThread.SetApartmentState(ApartmentState.STA);
+                    staThread.Start();
+                    staThread.Join();
+                    return clipboardData;
+                } catch (Exception exception) {
+                    return string.Empty;
                 }
             }
         }
