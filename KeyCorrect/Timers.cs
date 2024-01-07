b69d1190a9334d5548a05ea164c479e38f96be80
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2024-01-07 20:12:35 +0100

    reformat

--- KeyCorrect/Timers.cs
+++ KeyCorrect/Timers.cs
@@ -1,30 +1,30 @@
 ﻿namespace KeyCorrect {
-	internal static class Timers {
-		internal class ClipboardTimer {
-			internal static void Start() {
-				CreateTimer(() => {
-					if (MainStatus.Active) {
-						return;
-					}
-					string ClipboardText = TextCopy.ClipboardService.GetText();
-					try {
-						if (ClipboardText == null) {
-							ClipboardText = string.Empty;
-						}
-					} catch (Exception ex) {
-						ClipboardText = string.Empty;
-					}
-					MainStatus.TextToWrite = ClipboardText;
-				}, 200);
-			}
-		}
+    internal static class Timers {
+        internal class ClipboardTimer {
+            internal static void Start() {
+                CreateTimer(() => {
+                    if (MainStatus.Active) {
+                        return;
+                    }
+                    string ClipboardText = TextCopy.ClipboardService.GetText();
+                    try {
+                        if (ClipboardText == null) {
+                            ClipboardText = string.Empty;
+                        }
+                    } catch (Exception ex) {
+                        ClipboardText = string.Empty;
+                    }
+                    MainStatus.TextToWrite = ClipboardText;
+                }, 200);
+            }
+        }
 
-		internal class KeyboardLayoutTimer {
-			internal static void Start() {
-				CreateTimer(() => {
-					MainStatus.KeyboardLayout = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName.ToLower();
-				}, 1 * 1000);
-			}
-		}
-	}
+        internal class KeyboardLayoutTimer {
+            internal static void Start() {
+                CreateTimer(() => {
+                    MainStatus.KeyboardLayout = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName.ToLower();
+                }, 1 * 1000);
+            }
+        }
+    }
 }
